5b71e9fad40d8d1dec72cdf0b846abcfb1b56881
ku <frank>
2016-03-17 10:51:33 +0800

    1. 新增第二個測試案例

--- PotterShoppingCart/PotterShoppingCart.Tests/PotterShoppingCart.cs
+++ PotterShoppingCart/PotterShoppingCart.Tests/PotterShoppingCart.cs
@@ -27,5 +27,24 @@ namespace PotterShoppingCart.Tests
             Assert.AreEqual(expected, target.TotalPrice);
         }
 
+        [TestMethod]
+        public void CalcFee_第一集1本_第二集1本_第三集0本_第四集0本_第五集0本()
+        {
+            //Arrange
+            var order = new List<Book>()
+            {
+                new Book() { EP = 1, Name = "哈利波特", SellPrice = 100 },
+                new Book() { EP = 2, Name = "哈利波特", SellPrice = 100 }
+            };
+            var target = new ShoppingCart();
+
+            var expected = 190;
+            //Act
+
+            target.CalcFee(order);
+            //Assert
+
+            Assert.AreEqual(expected, target.TotalPrice);
+        }
     }
 }
